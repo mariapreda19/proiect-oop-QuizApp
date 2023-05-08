d93159f7cac12ce6a5d7211aa10a8380afdd50d1
maria <mariapreda1903@gmail.com>
2023-05-08 13:09:18 +0300

    valgrind

--- src/Erori.cpp
+++ src/Erori.cpp
@@ -5,13 +5,13 @@
 #include "../headers/Erori.h"
 
 eroare_fisier::eroare_fisier(const std::string& mesaj) :
-    std::runtime_error("eroare fisier: " + mesaj) {}
+    std::runtime_error(std::string("eroare fisier: ") + mesaj) {}
 
 eroare_fereastra::eroare_fereastra(const std::string& mesaj) :
-    std::runtime_error("eroare fereastra: " + mesaj) {}
+    std::runtime_error(std::string("eroare fereastra: ") + mesaj) {}
 
 eroare_imagine::eroare_imagine(const std::string& mesaj) :
-    eroare_fereastra("Nu a fost gasita o imagine " + mesaj) {}
+    eroare_fereastra(std::string("Nu a fost gasita o imagine ") + mesaj) {}
 
 eroare_font::eroare_font(const std::string& mesaj) :
-    eroare_fereastra("Nu a fost gasit fontul" + mesaj) {}
+    eroare_fereastra(std::string("Nu a fost gasit fontul") + mesaj) {}
